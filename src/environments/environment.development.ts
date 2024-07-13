892b33c72f691708603eb048f3c0045e17f59449
melangoth <septemberday@gmail.com>
2024-07-13 02:23:28 +0200

    switched to local test auth client id

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  google_auth_client_id: '175594743423-b9pf7sduf4pap78mkehr5okpqvhdrheb.apps.googleusercontent.com',
+  google_auth_client_id: '175594743423-3ghandpuid1eb50cqc2s39hgvffrsq4u.apps.googleusercontent.com',
   warlords_backend_url: 'http://localhost:8088'
 };
