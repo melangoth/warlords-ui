fe9fe23c709d17440ed6bfbd8fb8bb43948d9705
melangoth <septemberday@gmail.com>
2024-07-15 21:32:28 +0200

    added token to secure route but it's not valid

--- src/app/free-route-one/free-route-one.component.ts
+++ src/app/free-route-one/free-route-one.component.ts
@@ -1,4 +1,7 @@
 import {Component} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {CurrentUserService} from "../services/current-user.service";
 
 @Component({
   selector: 'app-free-route-one',
@@ -6,5 +9,19 @@ import {Component} from '@angular/core';
   styleUrls: ['./free-route-one.component.scss']
 })
 export class FreeRouteOneComponent {
+  constructor(http: HttpClient, user: CurrentUserService) {
 
+    const backend = environment.warlords_backend_url;
+
+    let token = user.getCurrentUser()?.token;
+    const headers = {'Authorization': `Bearer ${token}`};
+
+    http.get(`${backend}/api/user/free`).subscribe(response => {
+      console.log('free route accessed', response);
+    });
+
+    http.get(`${backend}/api/user/secure`, {headers}).subscribe(response => {
+      console.log('secure route accessed', response);
+    });
+  }
 }
